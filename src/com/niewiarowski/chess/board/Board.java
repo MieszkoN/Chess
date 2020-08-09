c8b5b91d328203f2d11504786a7928549e286c52
Mieszko <mieszkoniewiarowski@wp.pl>
2020-08-09 15:32:58 +0200

    add basic board implementation

--- src/com/niewiarowski/chess/board/Board.java
+++ src/com/niewiarowski/chess/board/Board.java
@@ -1,7 +1,52 @@
 package com.niewiarowski.chess.board;
 
+import com.niewiarowski.chess.ChessColor;
+import com.niewiarowski.chess.pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Board {
+    private final List<Tile> gameBoard;
+
+    public Board(final Builder builder) {
+        this.gameBoard = createGameBoard(builder);
+    }
+
+    private static List<Tile> createGameBoard(final Builder builder) {
+        final List<Tile> tiles = new ArrayList<>();
+        for(int i = 0; i < 64; i++) {
+            tiles.add(Tile.createTile(i, builder.boardConfiguration.get(i)));
+        }
+        return tiles;
+    }
+
     public Tile getTile(final int tileCoordinate) {
         return null;
+    }
+    
+    
+    public static class Builder {
+        Map<Integer, Piece> boardConfiguration;
+        ChessColor nextMoveMaker;
+
+        public Builder() {
+
+        }
+
+        public Builder setPiece(final Piece piece) {
+            this.boardConfiguration.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public Builder setMoveMaker(ChessColor nextMoveMaker) {
+            this.nextMoveMaker = nextMoveMaker;
+            return this;
+        }
+
+        public Board build() {
+            return new Board(this);
+        }
     }
 }
